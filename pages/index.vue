3643eba5b705cc7c6a65ca134d9fe32897b885e6
이지현 <leedmswl123@gmail.com>
2022-11-26 23:39:47 +0900

    프로젝트 기본구조 1차 세팅

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <h1>POC TOY PROJECT - 2022</h1>
+    <div class="button-wrap">
+      <span><nuxt-link to="auth/login">로그인</nuxt-link></span>
+      <spa><nuxt-link to="auth/join">회원가입</nuxt-link></spa>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layouts: 'IndexLayout',
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  background-color: antiquewhite;
+}
+</style>
