cafa37d893f64c6758b9343db481b4eb47735189
편도걸 <pdg2491@naver.com>
2022-11-30 02:09:31 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   apps: [
     {
-      name: 'wello-b2b-front-nuxt',
+      name: 'poc',
       script: 'npm',
       args: 'run start',
       autorestart: true,
@@ -13,12 +13,12 @@ module.exports = {
       kill_timeout: 5000,
       max_memory_restart: '500M',
       time: true,
-      merge_logs: true,
+      // merge_logs: true,
       log_type: 'raw',
       HOST: '0.0.0.0',
       port: 9001,
-      output: './logs/console.log',
-      error: './logs/consoleError.log',
+      // output: './logs/console.log',
+      // error: './logs/consoleError.log',
       env_stage: {
         NODE_ENV: 'development',
         HOST: '0.0.0.0',
