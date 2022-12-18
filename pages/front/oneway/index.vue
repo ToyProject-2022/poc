2c3c4311a5c92079deee197bed7fd344418539ed
편도걸 <pdg2491@naver.com>
2022-12-19 03:51:23 +0900

    oneway test

--- pages/front/oneway/index.vue
+++ pages/front/oneway/index.vue
@@ -16,7 +16,15 @@
       </div>
     </header>
     <div class="preview-area flex y-center">
-      <div class="preview"></div>
+      <div class="preview" :style="[{ backgroundColor: `${selectedColorHex}` }]">
+        <img
+          v-for="item in selectedItemList"
+          :key="item.itemId"
+          :src="item.fileUrl"
+          :alt="item.originName"
+          :style="[{ zIndex: item.zIndex }]"
+        />
+      </div>
     </div>
     <article class="combiner-area">
       <ul class="category-area flex x-start y-center">
@@ -41,7 +49,17 @@
             @click="handleClickColorItem(item)"
           ></li>
         </ul>
-        <div class="item-list"></div>
+        <ul class="item-list">
+          <li
+            v-for="item in itemList"
+            :key="item.itemId"
+            class="item"
+            :class="[{ 'is-active': selectedItemId === item.itemId }]"
+            @click="handleClickItem(item)"
+          >
+            <img :src="item.fileUrl" :alt="item.originName" />
+          </li>
+        </ul>
       </div>
     </article>
   </section>
@@ -60,7 +78,10 @@ export default {
       selectedTabId: 0,
       colorList: [],
       selectedColorId: 0,
+      selectedColorHex: '#fff',
       itemList: [],
+      selectedItemId: 0,
+      selectedItemList: [],
     }
   },
   created() {
@@ -87,11 +108,26 @@ export default {
         this.colorList = [...result, { itemCategoryDefaultColorId: -1, color: null }]
       }
     },
+    async getItemList() {
+      const result = await this.$_axios.$get(`/poc/v1/item`, {
+        params: {
+          itemCategoryId: this.selectedTabId,
+          page: 1,
+          size: 10,
+        },
+      })
+      if (result && result.contents && result.contents.length > 0) {
+        this.itemList = [...result.contents]
+      } else {
+        this.itemList = []
+      }
+    },
     async handleClickTabItem(item) {
       this.selectedTabId = item.itemCategoryId
       if (item.itemCategoryId > 0) {
         await this.getColorList(item.itemCategoryId)
         await this.handleClickColorItem(this.colorList[0])
+        await this.getItemList()
       } else {
         console.log('1')
       }
@@ -99,11 +135,16 @@ export default {
     handleClickColorItem(item) {
       console.log('### :: ', item)
       this.selectedColorId = item.itemCategoryDefaultColorId
+      this.selectedColorHex = `#${item.color}`
       if (item.itemCategoryDefaultColorId > 0) {
         console.log('1')
       } else {
         console.log('2')
       }
+    },
+    handleClickItem(item) {
+      this.selectedItemId = item.itemId
+      this.selectedItemList.push(item)
     },
   },
 }
@@ -124,27 +165,40 @@ export default {
     right: 0;
     height: calc(32px + 40px);
   }
+
   .preview-area {
     width: 100%;
     height: calc(100vh - 40%);
+
     .preview {
+      position: relative;
       width: min(100vw, 100%);
       height: min(100vw, 100%);
-      background: #97a6e7;
+      > img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: auto;
+      }
     }
   }
+
   .combiner-area {
     width: 100%;
-    height: 40%;
+    height: calc(100vh - 60%);
+
     .category {
       &-area {
         box-sizing: border-box;
         position: relative;
-        overflow-x: auto;
+        overflow: auto hidden;
         background-color: #fff;
         transition: all ease 0.2s;
         border-top: 1px solid #ccc;
+        height: 40px;
       }
+
       &-item {
         position: relative;
         height: 40px;
@@ -173,20 +227,59 @@ export default {
 
     .contents-area {
       position: relative;
-      padding: 8px 8px;
-      overflow-x: auto;
-      border-top: 3px solid #ccc;
-      border-bottom: 1px solid #ccc;
-      .color-item {
-        flex-shrink: 0;
-        border: 1px solid #ccc;
-        width: 28px;
-        height: 28px;
-        border-radius: 50%;
-        margin: 0 8px;
+      height: calc(100% - 40px);
 
-        &.is-active {
-          border-color: #6787ff;
+      .color {
+        &-list {
+          height: 48px;
+          padding: 8px 8px;
+          border-top: 3px solid #ccc;
+          border-bottom: 1px solid #ccc;
+          overflow: auto hidden;
+        }
+
+        &-item {
+          flex-shrink: 0;
+          border: 1px solid #ccc;
+          width: 28px;
+          height: 28px;
+          border-radius: 50%;
+          margin: 0 8px;
+
+          &.is-active {
+            border-color: #6787ff;
+          }
+        }
+      }
+
+      .item-list {
+        height: calc(100% - 48px);
+        padding: 8px;
+        overflow-y: auto;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: 84px;
+        justify-items: center;
+
+        .item {
+          width: 70px;
+          height: 70px;
+          flex-shrink: 0;
+          border: 1px solid #ccc;
+          border-radius: 10px;
+          margin: 8px;
+          overflow: hidden;
+          background-color: #ececec;
+
+          &.is-active {
+            border-color: #6787ff;
+          }
+
+          > img {
+            object-fit: fill;
+            width: 100%;
+            max-height: 100%;
+          }
         }
       }
     }
